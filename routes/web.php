722fb2c1d6dc635ebb3e33d7e9470a97d8751148
IsmoilObidov <84452090+IsmoilObidov@users.noreply.github.com>
2022-06-21 12:49:00 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Product;
+use App\Http\Controllers\Client;
+
+use App\Http\Controllers\Domain;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Route::get('/domain', [Domain::class, 'sds']);
+
+Route::get('/', [Domain::class, 'store'])->name('domain');
+
+Route::get('/check_domain', [Domain::class, 'check_domain'])->name('check_domain');
